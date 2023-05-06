7ab0a4b2e2303f1c123258e59404a7558919a93f
EL MEHDI AIT FAKIR <elmehdiaitfakir@gmail.com>
2023-05-06 01:01:17 +0100

    first commit

---
+++ src/components/PageHome.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+  hello from home page
+</div>
+  <div v-for="thread in threads" :key="thread.id">
+
+  </div>
+</template>
+
+<script>
+import sourceData from '@/data.json'
+
+export default {
+  name: "PageHome",
+  data (){
+    return {
+      posts: sourceData.posts,
+      threads: sourceData.threads,
+      users: sourceData.users
+    }
+  },
+  methods:{
+    postById(postId){
+      return this.posts.find(p => p.id === postId)
+    },
+    userById(userId){
+      return this.users.find(p => p.id === userId)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
