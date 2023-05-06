97b6e7f52a3efc3e4f8aa606a715604da089832f
EL MEHDI AIT FAKIR <elmehdiaitfakir@gmail.com>
2023-05-06 01:40:54 +0100

    displying a list of elements

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -3,7 +3,11 @@
   hello from home page
 </div>
   <div v-for="thread in threads" :key="thread.id">
-
+    <h2>{{thread.title}}</h2>
+    <div v-for="postId in thread.posts" :key="postId">
+      <p>{{userById(postById(postId).userId).name}}</p>
+      <p>{{postById(postId).text}}</p>
+    </div>
   </div>
 </template>
 
