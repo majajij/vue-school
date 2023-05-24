2975af29f65ae0b6413466a2ee585e1161d0afcb
EL MEHDI AIT FAKIR <elmehdiaitfakir@gmail.com>
2023-05-24 23:18:51 +0100

    add post list component

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -4,7 +4,7 @@
       <h1 class="text-6xl font-bold">404</h1>
       <p class="mt-4 text-xl text-gray-600">Oops! Page not found</p>
       <p class="mt-2 text-gray-500">The page you are looking for does not exist.</p>
-      <a href="/public" class="inline-block mt-4 px-6 py-3 text-white bg-primary rounded-lg shadow-md hover:bg-accent hover:text-black">Go back to homepage</a>
+      <router-link :to="{name:'Home'}" class="inline-block mt-4 px-6 py-3 text-white bg-primary rounded-lg shadow-md hover:bg-accent hover:text-black">Go back to homepage</router-link>
     </div>
   </div>
 </template>
--- src/pages/ThreadShow.vue
+++ src/pages/ThreadShow.vue
@@ -1,6 +1,7 @@
 <script>
 import {defineComponent} from 'vue'
 import sourceData from "@/data.json";
+import PostList from "@/components/PostList.vue";
 export default defineComponent({
   name: "ThreadShow",
   props:{
@@ -9,25 +10,24 @@ export default defineComponent({
       type: String
     }
   },
+  components:{
+    PostList
+  },
   data (){
     return {
       posts: sourceData.posts,
-      threads: sourceData.threads,
-      users: sourceData.users
+      threads: sourceData.threads
     }
   },
   computed: {
     thread(){
       return this.threads.find(t => t.id === this.id);
+    },
+    threadPosts(){
+      return this.posts.filter(p => p.threadId === this.id)
     }
   },
   methods:{
-    postById(postId){
-      return this.posts.find(p => p.id === postId)
-    },
-    userById(userId){
-      return this.users.find(p => p.id === userId)
-    }
   }
 })
 </script>
@@ -36,19 +36,7 @@ export default defineComponent({
 <div>
   <div class="mx-40">
     <h2 class="text-center font-bold text-2xl">{{thread.title}}</h2>
-    <div v-for="postId in thread.posts" :key="postId" class="flex bg-white mx-2 my-2 p-3 rounded-2xl">
-      <div class="flex w-2/12 flex-col justify-center items-center text-center">
-        <div class="text-teal-600 truncate w-20">{{userById(postById(postId).userId).name}}</div>
-        <div class="flex -space-x-2 overflow-hidden">
-          <img class="inline-block h-10 w-10 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-        </div>
-        <div class="italic">100 posts</div>
-      </div>
-      <div class="flex-initial w-10/12 px-2 py-2">
-        <p>{{postById(postId).text}}</p>
-        <span class="float-right mt-2 italic">{{postById(postId).publishedAt}}</span>
-      </div>
-    </div>
+    <post-list :posts="threadPosts" />
   </div>
 </div>
 </template>
